2c2d86e7983651f8412057eb924295a36a707664
pintoko-daruadi <pintoko.mpd@gmail.com>
2019-07-21 22:01:52 +0700

    Late Payment List page

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ CMS untuk manajemen data pencatatan rumah, penghuni, dan pembayaran.
 Project ini bertujuan untuk pembelajaran.
 
 ## TODO
+- Reporting per month
 - Search box in user identity
-- List of User who have not pay rent
-- List of House that empty
-- List payment by user
+- Filter of House that empty
+- Filter payment by user
 - Family Head and Member relationship through person
--- house/views.py
+++ house/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import LatepaymentForm
+from .models import Payment, Rent
 
 # Create your views here.
+def latepayment(request):
+	if 'month' in request.GET:
+		form = LatepaymentForm(request.GET)
+		if form.is_valid():
+			rent_ids = [payment.rent.id for payment in Payment.objects.filter(start__month=form.cleaned_data['month'], start__year=form.cleaned_data['year'])]
+			not_paid_rent = Rent.objects.filter(active=True, start_date__month__lte=form.cleaned_data['month'], start_date__year__lte=form.cleaned_data['year']).exclude(id__in = rent_ids)
+	else:
+		not_paid_rent = {}
+		form = LatepaymentForm()
+	return render(request, 'house/latepayment.html', {'form': form, 'data': not_paid_rent})
