58e28a90c9916ff856cf9e31a61d1f68f0d68d70
Pintoko Daruadi <pintoko.mpd@gmail.com>
2020-12-21 18:32:47 +0700

    remove unused form

--- house/forms.py
+++ house/forms.py
@@ -3,12 +3,9 @@ from django.utils.dates import MONTHS
 from datetime import date
 
 class LatepaymentForm(forms.Form):
-	years = [(x, str(x)) for x in range(2019, (date.today().year+1))]
+	START_SAVE_DATA = 2021
+	years = [(x, str(x)) for x in range(START_SAVE_DATA, (date.today().year+1))]
 	year = forms.ChoiceField(choices=years, widget=forms.Select(attrs={'onchange':'this.form.submit()'}))
 	
 	months = [(x, MONTHS[x]) for x in range(1, 13)]
 	month = forms.ChoiceField(choices=months, widget=forms.Select(attrs={'onchange':'this.form.submit()'}))
-
-class LoginForm(forms.Form):
-	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Username'}))
-	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))	
