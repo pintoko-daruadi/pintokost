e8876907b5bd2433bdc8f15ebdea989bb531f344
Pintoko Daruadi <pintoko.mpd@gmail.com>
2020-12-26 21:19:58 +0700

    remove permission for non admin

--- profile/admin.py
+++ profile/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext, ugettext_lazy as _
 from .models import Profile
 from .forms import UserCompleteNameField
-from django.utils.safestring import mark_safe
 
 class ProfileInline(admin.StackedInline):
 	model = Profile
@@ -42,7 +43,25 @@ class ProfileAdmin(UserAdmin):
 		except:
 			img_url = '/media/default.jpg'
 
-		return mark_safe('<img src="{url}" width="300px" alt="photo ktp" />'.format(url= img_url))
+		return mark_safe('<img src="{url}" width="250px" alt="photo ktp" />'.format(url= img_url))
+
+	def get_fieldsets(self, request, obj=None):
+		if not obj:
+			return self.add_fieldsets
+
+		if request.user.is_superuser:
+			perm_fields = ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+		else:
+			# modify these to suit the fields you want your
+			# staff user to be able to edit
+			perm_fields = ('is_active', 'groups')
+
+		return [
+				(None, {'fields': ('username', 'password')}),
+				(_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+				(_('Permissions'), {'fields': perm_fields}),
+				(_('Important dates'), {'fields': ('last_login', 'date_joined')})
+		]
 
 admin.site.unregister(User)
 admin.site.register(User, ProfileAdmin)
