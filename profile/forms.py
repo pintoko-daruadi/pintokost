c0703e6f7c0b79dd1fc3cfe5b416c2cd9cd3f99a
Pintoko Daruadi <pintoko.mpd@gmail.com>
2020-12-26 14:53:53 +0700

    add new renter user

--- profile/forms.py
+++ profile/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from .models import Profile
 
 class UserCompleteNameField(forms.ModelChoiceField):
 	def label_from_instance(self, obj):
 		return "%s %s - %s" % (obj.first_name, obj.last_name, obj.username)
+
+class RenterForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['nik', 'occupation', 'phone']
+
+	first_name = forms.CharField(label='Nama Depan', required=True)
+	last_name = forms.CharField(label='Nama Belakang', required=True)
+
+	field_order = ['nik', 'first_name', 'last_name', 'phone', 'occupation']
