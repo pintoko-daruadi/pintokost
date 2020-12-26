c4da6b8b03ca702a5d2ebbc3c58524d92d663c1d
Pintoko Daruadi <pintoko.mpd@gmail.com>
2020-12-26 12:00:40 +0700

    change step number of price

--- house/forms.py
+++ house/forms.py
@@ -18,7 +18,7 @@ class LatepaymentForm(forms.Form):
 	START_SAVE_DATA = 2021
 	years = [(x, str(x)) for x in range(START_SAVE_DATA, (date.today().year+1))]
 	year = forms.ChoiceField(choices=years, widget=forms.Select(attrs={'onchange':'this.form.submit()'}))
-	
+
 	months = [(x, MONTHS[x]) for x in range(1, 13)]
 	month = forms.ChoiceField(choices=months, widget=forms.Select(attrs={'onchange':'this.form.submit()'}))
 
@@ -29,3 +29,7 @@ class RentForm(forms.ModelForm):
 		widgets = {
 			'renter': PenghuniWidget,
 		}
+
+	def __init__(self, *args, **kwargs):
+		super(RentForm, self).__init__(*args, **kwargs)
+		self.fields['price'].widget.attrs = {'step': '1000'}
