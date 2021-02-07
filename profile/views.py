fd36d58a63ba8e58399736350d8ccb2d7921813a
Pintoko Daruadi <pintoko.mpd@gmail.com>
2021-02-07 21:31:44 +0700

    add mixin for checking non login user

--- profile/views.py
+++ profile/views.py
@@ -1,12 +1,15 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
 from .forms import LandlordSignupForm, RenterForm
 
-class LandlordSignupView(CreateView):
+class LandlordSignupView(UserPassesTestMixin, CreateView):
 	form_class = LandlordSignupForm
 	template_name = 'profile/landlord_signup.html'
 	success_url = reverse_lazy('house:list')
+
+	def test_func(self):
+		return self.request.user.is_authenticated == False
 
 class ProfileCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
 	permission_required = 'auth.add_user'
