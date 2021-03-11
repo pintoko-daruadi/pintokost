91759b604121abb1c97ffe378a6c146cdb3e4f21
Pintoko Daruadi <pintoko.mpd@gmail.com>
2021-03-11 21:35:16 +0700

    update filter

--- house/templatetags/payment_filter.py
+++ house/templatetags/payment_filter.py
@@ -7,9 +7,12 @@ from django.utils.html import format_html
 @register.filter(name='remain')
 def remain(payment, price):
     html = "<p><i class='fa fa-tags' style='color: var(--orange)'></i> {}</p>".format(toRupiah(price))
+    html += "<i class='fa fa-exclamation-triangle' style='color: var(--red)'></i> Cicilan:<ul>"
     if payment:
-        html += "<i class='fa fa-exclamation-triangle' style='color: var(--red)'></i> Cicilan:<ul>"
         for p in payment:
-            html += "<li class='fa fa-tag' style='color: var(--primary)'> {date} &rsaquo; {nominal}</li>".format(date=p.pay_date.strftime("%d-%b-%Y"), nominal=toRupiah(p.nominal))
-        html += "</ul>"
+            html += "<li style='color: var(--primary)'> {date} &rsaquo; {nominal}</li>".format(date=p.pay_date.strftime("%d-%b-%Y"), nominal=toRupiah(p.nominal))
+    else:
+        html += "<li><i> Belum ada pembayaran</i></li>"
+    html += "</ul>"
+
     return format_html(mark_safe(html))
