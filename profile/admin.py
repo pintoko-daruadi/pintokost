2a7678c07550d03c18a04706927667a9726e37ef
Pintoko <pintoko.mpd@gmail.com>
2022-02-10 08:16:34 +0700

    change name of deprecated function

--- profile/admin.py
+++ profile/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import User, Group
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from .models import Profile
 from .forms import UserCompleteNameField
 
